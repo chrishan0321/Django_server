76da018050e486908b197ee1d99ea1ffd6c4fe7a
chrishan0321 <hant20110321@gmail.com>
2024-05-07 21:57:28 +0900

    Feat: 유저 데이터 db 저장, 불러오기

--- hacking/main/views.py
+++ hacking/main/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
 # def 함수이름(인자):
 #   함수를 호출했을 때 실행될 코드
 # Create your views here.
 def showmain(request):
-    return render(request, "main/mainpage.html")
+    return render(request, "mainpage.html")
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        pwd = request.POST.get("pwd")
+        User.objects.create(username=username, pwd=pwd)
+        return HttpResponse(f"welcome! {username}")
+    return
+
+def getUserData(request):
+    queryset = User.objects.all()
+    print(queryset)
